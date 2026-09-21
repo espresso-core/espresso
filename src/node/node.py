7f70969de09c2ce93ab67c5c64fe698ac794cf92
espresso-core <espresso.bean@outlook.com>
2026-09-21 11:33:05 -0400

    First node
    - added node logo
    - added first iteration of node

---
+++ src/node/node.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify
+ 
+app = Flask(__name__)
+ 
+balances = {
+    "fb82a34bdd491703f4935cd963505af2dd9d0f8f": 12.5,
+    "def456": 100.0
+}
+ 
+@app.route("/balance/<address>")
+def get_balance(address):
+    balance = balances.get(address, 0)
+ 
+    return jsonify({
+        "address": address,
+        "balance": balance
+    })
+
+app.run()
